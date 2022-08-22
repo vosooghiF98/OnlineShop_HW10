e0d180edc50a887f18e83cfbdbf47de7a91159ea
farshid vosooghi <vosooghifarshid95@gmail.com>
2022-08-22 16:27:52 +0430

    create Exception package create BoundException & InputException & SignUpSignInException

---
+++ src/main/java/org/maktab/Exception/BoundException.java
@@ -0,0 +1,7 @@
+package org.maktab.Exception;
+
+public class BoundException extends RuntimeException{
+    public BoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/org/maktab/Exception/SignUpSignInException.java
@@ -0,0 +1,7 @@
+package org.maktab.Exception;
+
+public class SignUpSignInException extends RuntimeException{
+    public SignUpSignInException(String message) {
+        super(message);
+    }
+}
