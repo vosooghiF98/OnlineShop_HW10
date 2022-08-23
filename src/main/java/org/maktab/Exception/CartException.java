aa9ca9fd243cdb65c5fc412a66b93099f76937b0
farshid vosooghi <vosooghifarshid95@gmail.com>
2022-08-23 15:43:26 +0430

    create CartException & InventoryException

---
+++ src/main/java/org/maktab/Exception/CartException.java
@@ -0,0 +1,7 @@
+package org.maktab.Exception;
+
+public class CartException extends RuntimeException{
+    public CartException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/org/maktab/Exception/InventoryException.java
@@ -0,0 +1,7 @@
+package org.maktab.Exception;
+
+public class InventoryException extends RuntimeException{
+    public InventoryException(String message) {
+        super(message);
+    }
+}
