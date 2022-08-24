7c84159e7c478f79b185248420017de5c3abcc62
farshid vosooghi <vosooghifarshid95@gmail.com>
2022-08-24 20:59:15 +0430

    update View class

--- src/main/java/org/maktab/View/View.java
+++ src/main/java/org/maktab/View/View.java
@@ -6,31 +6,31 @@ import java.util.Scanner;
 
 public class View {
     Check check = new Check();
-    public int main(Scanner input){
+    public int main(){
         System.out.println("Menu :");
         System.out.println("User : 1");
         System.out.println("Admin : 2");
         System.out.println("Exit : 3");
-        return check.checkButton(1,3,input);
+        return check.checkButton(1,3);
     }
 
-    public int user(Scanner input){
+    public int user(){
         System.out.println("User Menu : ");
         System.out.println("Sign Up : 1");
         System.out.println("Sign In : 2");
         System.out.println("Exit : 3");
-        return check.checkButton(1,3,input);
+        return check.checkButton(1,3);
     }
 
-    public int admin(Scanner input){
+    public int admin(){
         System.out.println("Admin Menu : ");
         System.out.println("Sign Up : 1");
         System.out.println("Sign In : 2");
         System.out.println("Exit : 3");
-        return check.checkButton(1,3,input);
+        return check.checkButton(1,3);
     }
 
-    public int cart(Scanner input){
+    public int cart(){
         System.out.println("Menu :");
         System.out.println("Add Product To Your Cart : 1");
         System.out.println("Remove Product From Your Cart : 2");
@@ -40,15 +40,15 @@ public class View {
         System.out.println("Show Shop Products : 6");
         System.out.println("Accept Payment : 7");
         System.out.println("Edit username & password : 8");
-        System.out.println("Remove Account : 9");
-        System.out.println("Exit : 10");
-        return check.checkButton(1,10,input);
+        System.out.println("Exit : 9");
+        return check.checkButton(1,9);
     }
 
-    public int shop(Scanner input){
+    public int shop(){
         System.out.println("Menu :");
         System.out.println("Add Product To Shop : 1");
-        System.out.println("Exit : 2");
-        return check.checkButton(1,2,input);
+        System.out.println("Edit username & password : 2");
+        System.out.println("Exit : 3");
+        return check.checkButton(1,3);
     }
 }
